370a1a4804ab132294fd011825271f9be2f72a16
RihabMhd <rihabmahdi19@gmail.com>
2026-02-10 19:00:55 +0100

    fix:fixed cart when the user is not logged in

--- storage/framework/views/3faa8ede02792426a6ad055c8eef9ee1.php
+++ storage/framework/views/3faa8ede02792426a6ad055c8eef9ee1.php
@@ -239,7 +239,7 @@
                                 d="M10.5 10h4m-2-2v4m4 9a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3m-8 0a1.5 1.5 0 1 0 0-3a1.5 1.5 0 0 0 0 3M3.71 5.4h15.214c1.378 0 2.373 1.27 1.995 2.548l-1.654 5.6C19.01 14.408 18.196 15 17.27 15H8.112c-.927 0-1.742-.593-1.996-1.452zm0 0L3 3" />
                         </svg>
                         <?php
-                            $cart = auth()->user()->cart;
+                            $cart = auth()->check() ? auth()->user()->cart : null;
                             $count = $cart ? $cart->getTotalItems() : 0;
                         ?>
                         <?php if($count > 0): ?>
@@ -588,7 +588,7 @@
                     <?php echo e(__('Cart')); ?>
 
                     <?php
-                        $cart = auth()->user()->cart;
+                        $cart = auth()->check() ? auth()->user()->cart : null;
                         $count = $cart ? $cart->getTotalItems() : 0;
                     ?>
                     <?php if($count > 0): ?>
@@ -748,4 +748,5 @@
             </div>
         <?php endif; ?>
     </div>
-</nav><?php /**PATH C:\laragon\www\TifawinSouk-E-com\resources\views/layouts/navigation.blade.php ENDPATH**/ ?>
+</nav>
+<?php /**PATH C:\laragon\www\TifawinSouk-E-com\resources\views/layouts/navigation.blade.php ENDPATH**/ ?>
